818c4058c0d91a087d85216bc220f0c03ec769df
LukasWestholt <support@lukaswestholt.de>
2022-04-16 04:00:23 +0200

    in work

--- betterhtwk2ical/urls.py
+++ betterhtwk2ical/urls.py
@@ -1,7 +1,27 @@
-from django.urls import path
+"""betterhtwk2ical URL Configuration
 
-from . import views
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls.i18n import i18n_patterns
+from django.urls import include, path
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),  # TODO Test
+    path('i18n/', include('django.conf.urls.i18n')),  # TODO Test https://docs.djangoproject.com/en/4.0/topics/i18n/translation/#the-set-language-redirect-view
+    path('', include('myapp1.unlocalize_urls'))
 ]
+urlpatterns += i18n_patterns(
+    path('', include('myapp1.urls')),
+)
--- config.py
+++ config.py
@@ -17,16 +17,16 @@ latest_valid_date = datetime.datetime(2022, 3, 31, 20)
 start_date = datetime.datetime(2022, 4, 4)
 start_week = 14
 
-# path to XML files for subjects and courses
-all_subjects_xml_url = "https://stundenplan.htwk-leipzig.de/stundenplan/xml/public/semgrp_ss.xml"
-all_courses_xml_url = "https://stundenplan.htwk-leipzig.de/stundenplan/xml/public/modul_ss.xml"
+# path to XML files for groups and modules
+all_groups_xml_url = ("https://stundenplan.htwk-leipzig.de/stundenplan/xml/public/semgrp_ss.xml", "utf-8")
+all_modules_xml_url = ("https://stundenplan.htwk-leipzig.de/stundenplan/xml/public/modul_ss.xml", "utf-8")
 
 # ID of XML node in 'all_courses_xml_url' that contains all studium generale
 # modules
 studium_generale_fakultaet_id = '%23SPLUS905495'
 
-# base path for single schedule per subject and studium generale
-single_subjects_html_url = "https://stundenplan.htwk-leipzig.de/ss/Berichte/Text-Listen;Studenten-Sets;name;" \
-                           "###SLUG###?template=UNEinzelGru&weeks=14-35"
-studium_generales_html_url = "https://stundenplan.htwk-leipzig.de/ss/Berichte/Text-Listen;Module;id;" \
-                             "###SLUG###?template=UNEinzelLV&weeks=14-35"
+# base path for single schedule per group and studium generale
+single_group_html_url = ("https://stundenplan.htwk-leipzig.de/ss/Berichte/Text-Listen;Studenten-Sets;name;"
+                         "###SLUG###?template=UNEinzelGru&weeks=14-35", "ISO-8859-1")
+studium_generale_html_url = ("https://stundenplan.htwk-leipzig.de/ss/Berichte/Text-Listen;Module;id;" 
+                             "###SLUG###?template=UNEinzelLV&weeks=14-35", "ISO-8859-1")
