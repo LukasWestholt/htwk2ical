fbda83f65c9928b089d1c1c0dc3f51df4391e51e
LukasWestholt <support@lukaswestholt.de>
2022-06-10 18:58:28 +0200

    forms.py history

--- myapp1/forms.py
+++ myapp1/forms.py
@@ -1,57 +1,83 @@
 #!/usr/bin/env python3
 # coding: utf-8
 from django import forms
-from django.forms import BaseFormSet
 from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet, formset_factory, BaseFormSet
 
 
-class BaseCalendarFormSet(BaseFormSet):
-    def get_form_kwargs(self, index):
-        kwargs = super().get_form_kwargs(index)
-        kwargs['index'] = index
-        return kwargs
+"""class MultiEmailField(forms.Field):
+    def to_python(self, value):
+        Normalize data to a list of strings.
+        # Return an empty list if no input was given.
+        if not value:
+            return []
+        return value.split(',')
 
+    def validate(self, value):
+        Check if value consists only of valid emails.
+        # Use the parent's handling of required fields, etc.
+        super().validate(value)
+        for email in value:
+            pass
+"""
+
+class CalendarItemForm(forms.Form):
+    module_aliases = forms.CharField(
+        label="", widget=forms.TextInput(
+            attrs={
+                "class":          "form-control",
+                "data-form-type": "other"
+            }
+        ))
+
+
+CalendarItemFormset = formset_factory(CalendarItemForm, extra=0)
+
+
+class CalendarFormset(BaseFormSet):
+    def add_fields(self, form, index):
+        super().add_fields(form, index)
+        # save the formset in the 'nested' property
+        form.nested = CalendarItemFormset(
+            initial=[{'module_aliases': module.name} for module in form.modules])
+            # initial=form.initial if form.is_bound else None)
+            # prefix='address-%s-%s' % (
+            # form.prefix,
+            # CalendarFormset.get_default_prefix()))
+        # print(form.nested)
 
 class CalendarForm(forms.Form):
     def __init__(self, *args, **kwargs):
         # initialize the form before creating fields
         super(CalendarForm, self).__init__(*args, **kwargs)
-        print(kwargs)
-        # index = kwargs.pop('index')
-        if 'groups' in kwargs.keys():
-            groups = kwargs.pop('groups')
-            # create the dynamic fields
-            group = groups[index]
+        # create the dynamic fields
+        if 'initial' in kwargs:
+            group = kwargs['initial']
             self.title = group["title"]
-            self.id = group["id"]
-            for module in group["modules"]:
+            self.id = group["group_id"]
+            self.modules = group["modules"]
+            """for module in group["modules"]:
                 self.fields["module_aliases[" + str(module.id) + "]"] = forms.CharField(
-                    label="", widget=forms.TextInput(
+                    label="", initial=module.name, widget=forms.TextInput(
                         attrs={
                             "class": "form-control",
-                            "data-form-type": "other",
-                            "value": module.name,
-                            "data-group": group["id"]
+                            "data-form-type": "other"
                         }
                     ))
-            self.fields["group_id"] = forms.CharField(
-                label="", widget=forms.HiddenInput(
-                    attrs={"value": group["id"]}
-                ))
+            """
         else:
-            # create the dynamic fields
-            group = {key.removeprefix(kwargs['prefix'] + "-"): kwargs['data'][key]
-                     for key in kwargs['data'].keys() if key.startswith(kwargs['prefix'] + "-")}
-            self.id = group.pop("group_id")
+            """group = {key.removeprefix(kwargs['prefix'] + "-"): kwargs['data'][key]
+                     for key in kwargs['data'] if key.startswith(kwargs['prefix'] + "-") and key != kwargs['prefix'] + "-group_id"}
             for module in group:
                 self.fields[module] = forms.CharField(
-                    label="", widget=forms.TextInput(
+                    label="", initial=group[module], widget=forms.TextInput(
                         attrs={
                             "class":          "form-control",
-                            "data-form-type": "other",
-                            "value":          group[module]
+                            "data-form-type": "other"
                         }
-                    ))
+                    ))"""
+            pass
+    group_id = forms.CharField(label="", widget=forms.HiddenInput())
 
     """def clean(self):
         if any(self.errors):
@@ -64,6 +90,6 @@ class CalendarForm(forms.Form):
             title = form.cleaned_data.get('title')
             if title in titles:
                 raise ValidationError("Articles in a set must have distinct titles.")
-            titles.append(title)
-    """
+            titles.append(title)"""
+
     # {{ form.field.as_hidden }}
