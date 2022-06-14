a6ba0f8d9253c6222effcc93889f83a934575b3d
LukasWestholt <support@lukaswestholt.de>
2022-06-14 13:34:12 +0200

    better init

--- myapp1/management/commands/init.py
+++ myapp1/management/commands/init.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+import os
 from django.core.management import BaseCommand, call_command
+from htwk2ical.settings import STATIC_ROOT
 
 
 class Command(BaseCommand):
     help = 'Init htwk2ical'
 
     def handle(self, *args, **kwargs):
-        call_command('set_donate_amount', '0.0')
-        call_command('rebuild_cache')
+        if not os.path.isdir(STATIC_ROOT) or not os.listdir(STATIC_ROOT):
+            call_command('collectstatic', clear=True, interactive=False)
+            call_command('set_donate_amount', '0.0')
+            call_command('rebuild_cache')
